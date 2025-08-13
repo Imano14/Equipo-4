b4538d23175225cdb24ed358ec73f1867bd1f4c2
324030188-cecii <324030188@upjr.edu.mx>
2025-08-13 12:24:34 -0600

    Update main.dart
    codigo main

--- inventario_project/lib/main.dart
+++ inventario_project/lib/main.dart
@@ -4,119 +4,193 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TextfieldGeneral extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+  final Function(String) onChanged;
+  final TextInputType keyboardType;
+  final IconData icon;
+  final bool obscureText;
+  final String? Function(String?)? validator;
 
-  // This widget is the root of your application.
+  const TextfieldGeneral({
+    required this.labelText,
+    required this.hintText,
+    required this.onChanged,
+    this.keyboardType = TextInputType.text,
+    required this.icon,
+    required this.obscureText,
+    this.validator,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(13.333),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      child: TextFormField(
+        keyboardType: keyboardType,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          labelText: labelText,
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(13.333),
+          ),
+          filled: true,
+          fillColor: Colors.white,
+        ),
+        onChanged: onChanged,
+        validator: validator,
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  static const String id = 'login_page';
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _nombre = '';
+  String _correo = '';
+  String _contrasena = '';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color(0xFFB71C1C),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Login'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Inventario \n JVK".toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFFFFD700),
+                    fontSize: 50,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: "MontaguSlab_24pt-Bold",
+                  ),
+                ),
+                // ...existing code...
+                CircleAvatar(
+                  radius: 75,
+                  backgroundColor: Colors.white,
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/images/inventario.png',
+                      height: 1000,
+                      width: 1000,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 40),
+
+                SizedBox(height: 0),
+                _textfieldName(),
+
+                SizedBox(height: 7),
+                _textfieldPassword(),
+                SizedBox(height: 7),
+
+                _buttonSingUp(context),
+                SizedBox(height: 7),
+
+                _buttomlogin(context),
+                SizedBox(height: 7),
+              ],
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _textfieldName() {
+    return SizedBox(
+      width: 900,
+      child: TextfieldGeneral(
+        labelText: "Usuario",
+        hintText: "Ingresa tu usuario",
+        icon: Icons.person_outline,
+        obscureText: false,
+        onChanged: (value) => _nombre = value,
+        validator:
+            (value) =>
+                value == null || value.isEmpty ? 'Campo requerido' : null,
+      ),
+    );
+  }
+
+  Widget _textfieldPassword() {
+    return SizedBox(
+      width: 900,
+      child: TextfieldGeneral(
+        labelText: "Contraseña",
+        hintText: "****",
+        icon: Icons.lock,
+        keyboardType: TextInputType.visiblePassword,
+        obscureText: true,
+        onChanged: (value) => _contrasena = value,
+        validator:
+            (value) =>
+                value == null || value.length < 6
+                    ? 'Mínimo 6 caracteres'
+                    : null,
+      ),
+    );
+  }
+
+  Widget _buttonSingUp(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Singup()),
+        );
+      },
+      child: Text("REGISTRARSE"),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFFFFD700), // Dorado
+        foregroundColor: Color(0xFFB71C1C),
+        textStyle: TextStyle(fontSize: 20),
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+  }
+
+  Widget _buttomlogin(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text("INICIAR SESIÓN"),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFF616161), // Gris metálico
+        foregroundColor: Colors.white,
+        textStyle: TextStyle(fontSize: 20),
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
     );
   }
 }
